3338eed80be21723cf879e94184ff7657f2618f2
daisuke shiraishi <kaiyujin@gmail.com>
2022-06-25 19:31:23 +0900

    Add echo

--- api/main.go
+++ api/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"github.com/labstack/echo/v4"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"net/http"
 	"time"
 )
 
@@ -10,5 +12,15 @@ func main() {
 	zerolog.TimeFieldFormat = time.RFC3339
 	log.Info().Msg("Start server")
 	defer log.Info().Msg("Stop server")
-	// TODO
+	// Router
+	e := echo.New()
+	e.GET("/", health)
+	log.Fatal().Err(e.Start(":80"))
+}
+
+func health(c echo.Context) error {
+	result := map[string]interface{}{
+		"status": "OK",
+	}
+	return c.JSON(http.StatusOK, result)
 }
